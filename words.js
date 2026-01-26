d423fea406b9f8b1b5db48320a56d8719ff21d86
pavelpolegenkiy-ctrl <pavelpolegenkiy@gmail.com>
2026-01-26 12:09:26 +0100

    ver-1.0

---
+++ words.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+// words.js
+const wordData = [
+    { id: 1, de: "der Hund", ru: "Собака" },
+    { id: 2, de: "die Katze", ru: "Кошка" },
+    { id: 3, de: "das Haus", ru: "Дом" },
+    { id: 4, de: "der Tisch", ru: "Стол" },
+    { id: 5, de: "das Buch", ru: "Книга" },
+    { id: 6, de: "die Schule", ru: "Школа" },
+    { id: 7, de: "der Apfel", ru: "Яблоко" },
+    { id: 8, de: "das Auto", ru: "Машина" },
+    { id: 9, de: "die Sonne", ru: "Солнце" },
+    { id: 10, de: "der Freund", ru: "Друг" },
+    { id: 11, de: "arbeiten", ru: "Работать" },
+    { id: 12, de: "lernen", ru: "Учиться" },
+    // Сюда можно добавлять сотни новых строк:
+    { id: 13, de: "rot", ru: "Красный" },
+    { id: 14, de: "blau", ru: "Синий" }
+];
